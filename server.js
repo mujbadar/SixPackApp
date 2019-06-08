ce552995c1eb3eb1e0f08d71116851c8983b8118
Nida Badar <nidabadar@Nidas-MacBook-Pro.local>
2019-06-08 12:15:23 -0500

    dotenv

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 const mongoose = require('mongoose')
 const session = require('express-session')
 const methodOverride = require('method-override')
+const env = require('dotenv').config()
 const db = mongoose.connection
 const PORT = process.env.PORT || 3000
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/sixpackapp'
